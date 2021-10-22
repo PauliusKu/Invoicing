fcfe5fb41b9969e5f7fe14b6ede6488e42923123
Paulius Kuprevicius <pakupr@ttu.ee>
2021-10-22 21:26:08 +0300

    EBFR011: Project structure initiated.

---
+++ test/ATMTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class ATMTest {
+    @Test
+    public void test() {
+    }
+
+}
