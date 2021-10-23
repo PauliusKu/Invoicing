46390d7fde76a3eb6d07426c244197f9e19d7fd4
Lukas Baltramaitis <lubalt@ttu.ee>
2021-10-23 16:19:50 +0300

    EBFR011: Alternative scenario test added [Fails].

--- test/Login.java
+++ test/Login.java
@@ -35,4 +35,28 @@ class Login {
 
     }
 
+    @Test
+    public void testLoginErrorScenario() {
+        MockLoginPrinter mockLoginPrinter = new MockLoginPrinter();
+        Config.INPUT_READER = new MockLoginReader("admin", "not_admin");
+        Config.OUTPUT_PRINTER = mockLoginPrinter;
+        Client.run();
+        List<String> outputs = mockLoginPrinter.getOutputs();
+        String loginWindowName = outputs.get(0);
+        String loginMessage = outputs.get(1);
+        String inputEmailText = outputs.get(2);
+        String inputPasswordText = outputs.get(3);
+        String mainMenuTitle = outputs.get(4);
+        String mainMenuMessage = outputs.get(5);
+        //then
+        Assertions.assertAll(
+                () -> assertEquals("Sign in", loginWindowName),
+                () -> assertEquals("Welcome to Invoicing!", loginMessage),
+                () -> assertEquals("Write your email:", inputEmailText),
+                () -> assertEquals("Write your password:", inputPasswordText),
+                () -> assertEquals("Sign in", mainMenuTitle),
+                () -> assertEquals("Error: [Wrong email or password]", mainMenuMessage)
+        );
+    }
+
 }
