cb75ba46859d4c57ae26d90d7e6ece7e70a66f92
PauliusKu <paulius.kuprevicius@gmail.com>
2021-10-23 19:37:38 +0300

    EBFR020: created empty clients controller.

--- src/main/controller/access/ClientsController.java
+++ src/main/controller/access/ClientsController.java
@@ -2,6 +2,13 @@ package main.controller.access;
 
 import main.controller.common.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ClientsController extends Controller {
 
+    public static List<List<String>> getClients(String token) {
+        return null;
+    }
 }
